0be99540db1635b75ab5f6555582c16faa24cf4b
frontendercode <frontendercode@gmail.com>
2021-10-16 17:15:25 +0300

    v.0.1
    Создана стартовая тема для посадки на WordPress, подготовлены файлы верстки

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # js-wordpress
-Посадка верстки шаблона js на WordPress + Woocommerce
+
+Выполняю посадку шаблона, скачаного в интернете на WordPress + Woocommerce. Для меня это сложная работа, потому что я не просто создаю тему под WordPress, а и адаптирую тему под поддержку плагина Woocommerce.
+
+
+## Необходимые плагины
+
+Classic Editor
+Cyr to Lat enhanced
+Query Monitor
+Show Current Template
+WooCommerce
+
+## Стартовая тема
+
+Была создана стартовая тема с сайта https://underscores.me/ c поддержкой WooCommerce. 
+
+
+
+
+## Связаться по вопросам создания сайта под ключ:
+<br>
+
+[![facebook](https://img.shields.io/badge/-Facebook-1877F2?style=for-the-badge&logo=Figma&logoColor=eeffff)](https://www.facebook.com/frontendercode)
+[![Telegram](https://img.shields.io/badge/-Telegram-26A5E4?style=for-the-badge&logo=Telegram&logoColor=eeffff)](https://t.me/frontendcoder)
+[![Instagram](https://img.shields.io/badge/-Instagram-E4405F?style=for-the-badge&logo=Instagram&logoColor=eeffff)](https://www.instagram.com/frontendercode/?hl=ru)
+[![GitHub](https://img.shields.io/badge/-GitHub-181717?style=for-the-badge&logo=GitHub&logoColor=eeffff)](https://github.com/frontend-coder)
+
+
+
+[![Portfolio](https://img.shields.io/badge/-Портфолио-181717?style=for-the-badge&logo=Internet-Archive&logoColor=eeffff)](https://frontend-coder.github.io)
+
---
+++ jst/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * jstheme Theme Customizer
+ *
+ * @package jstheme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function jst_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'jst_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'jst_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'jst_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function jst_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function jst_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function jst_customize_preview_js() {
+	wp_enqueue_script( 'jst-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'jst_customize_preview_js' );
